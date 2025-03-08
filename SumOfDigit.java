b07c581c67b2a6ea80f1b225683d88ffab8a9404
vibhor srivastava <vksbindaas5@gmail.com>
2025-03-08 20:07:44 +0530

    Update SumOfDigit.java

--- SumOfDigit.java
+++ SumOfDigit.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 public class SumOfDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        int sum = 0;
+        int a = n;
+
+        while(a>0){
+            int rem = a%10;
+            sum = sum + rem;
+            a = a/10;
+        }
+        System.out.println("The sum of digits is " + sum);
         
     }
     
 }
+
